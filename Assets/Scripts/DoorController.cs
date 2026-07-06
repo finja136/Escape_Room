9aadb7b193162a9f5bfba775c48e47af81143835
Luis Gronmayer <s1lugron@uni-trier.de>
2026-07-06 16:43:16 +0200

    Tür sound

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class DoorController : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip doorOpenSound;
 
     private bool opened = false;
 
@@ -14,5 +16,6 @@ public class DoorController : MonoBehaviour
 
         opened = true;
         animator.SetTrigger("Open");
+        audioSource.PlayOneShot(doorOpenSound);
     }
 }
